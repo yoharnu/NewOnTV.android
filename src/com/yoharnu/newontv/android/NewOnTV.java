f16de61390b885d779e8d08693c1bce14c88d2ec
yoharnu <dani91@gmail.com>
2012-12-14 13:54:18 -0500

    Shows with abnormal times now display

--- src/com/yoharnu/newontv/android/NewOnTV.java
+++ src/com/yoharnu/newontv/android/NewOnTV.java
@@ -8,6 +8,7 @@ import org.apache.commons.io.FileUtils;
 
 import com.yoharnu.newontv.android.shows.EditShowsList;
 import com.yoharnu.newontv.android.shows.Episode;
+import com.yoharnu.newontv.android.shows.Series;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -440,6 +441,76 @@ public class NewOnTV extends Activity {
 						}
 
 				}
+				{
+					final TextView time = new TextView(App.getContext());
+					time.setVisibility(View.GONE);
+					time.setText("Other Times");
+					runOnUiThread(new Runnable() {
+						public void run() {
+							ll.addView(time);
+						}
+					});
+					for (int i = 0; i < App.shows.size(); i++)
+						if (!(App.shows.get(i).getTime().replaceAll(" ", "")
+								.equalsIgnoreCase("8:00PM")
+								|| App.shows.get(i).getTime().matches("20:00")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("8:30PM")
+								|| App.shows.get(i).getTime().matches("20:30")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("9:00PM")
+								|| App.shows.get(i).getTime().matches("21:00")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("9:30PM")
+								|| App.shows.get(i).getTime().matches("21:30")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("10:00PM")
+								|| App.shows.get(i).getTime().matches("22:00")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("10:30PM")
+								|| App.shows.get(i).getTime().matches("22:30")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("11:00PM")
+								|| App.shows.get(i).getTime().matches("23:00")
+								|| App.shows.get(i).getTime()
+										.replaceAll(" ", "")
+										.equalsIgnoreCase("11:30PM") || App.shows
+								.get(i).getTime().matches("23:30"))) {
+							for (int j = 0; j < App.shows.get(i).episodes
+									.size(); j++) {
+								runOnUiThread(new Runnable() {
+									public void run() {
+										time.setVisibility(View.VISIBLE);
+									}
+								});
+								noShows = false;
+								try {
+									if (App.shows.get(i).episodes.get(j).task != null)
+										App.shows.get(i).episodes.get(j).task
+												.join();
+									final Episode temp = App.shows.get(i).episodes
+											.get(j);
+									final TextView tempView = new TextView(App
+											.getContext());
+									tempView.setText(App.shows.get(i).getTime());
+									runOnUiThread(new Runnable() {
+										public void run() {
+											ll.addView(tempView);
+											ll.addView(temp.print());
+										}
+									});
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+							}
+						}
+				}
 				if (noShows) {
 					final TextView nothing = new TextView(App.getContext());
 					nothing.setText("There is nothing new on tonight.");
@@ -483,56 +554,6 @@ public class NewOnTV extends Activity {
 			return true;
 		case R.id.menu_settings:
 			startActivity(new Intent(this, Settings.class));
-			return true;
-		case R.id.menu_export:
-			File src = new File(getFilesDir(), "shows");
-			File dir = new File(getExternalFilesDir(null), "NewOnTV");
-			if (!dir.exists()) {
-				dir.mkdir();
-			}
-			File dest = new File(dir, "shows.txt");
-			if (!dest.exists())
-				try {
-					dest.createNewFile();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			Toast toast = null;
-			try {
-				FileUtils.copyFile(src, dest);
-				toast = Toast.makeText(
-						App.getContext(),
-						"File successfully exported to "
-								+ dest.getAbsolutePath(), Toast.LENGTH_LONG);
-			} catch (IOException e) {
-				e.printStackTrace();
-				toast = Toast.makeText(App.getContext(),
-						dest.getAbsolutePath(), Toast.LENGTH_SHORT);
-			}
-			if (toast != null)
-				toast.show();
-			return true;
-		case R.id.menu_import:
-			dir = new File(getExternalFilesDir(null), "NewOnTV");
-			if (!dir.exists()) {
-				dir.mkdir();
-			}
-			src = new File(dir, "shows.txt");
-			dest = new File(getFilesDir(), "shows");
-			toast = null;
-			try {
-				FileUtils.copyFile(src, dest);
-				toast = Toast.makeText(
-						App.getContext(),
-						"File successfully imported from "
-								+ src.getAbsolutePath(), Toast.LENGTH_LONG);
-			} catch (IOException e) {
-				e.printStackTrace();
-				toast = Toast.makeText(App.getContext(),
-						"Failed to import file", Toast.LENGTH_SHORT);
-			}
-			if (toast != null)
-				toast.show();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
